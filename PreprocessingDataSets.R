ea9a6867b9b339c23125218ba4bd667156078374
Juan C. Correa <j.correa.n@gmail.com>
2021-06-24 12:42:01 -0500

    Disciplines

--- PreprocessingDataSets.R
+++ PreprocessingDataSets.R
@@ -8,11 +8,15 @@ library(dplyr)
 variable.names(LT)
 # Agro = agribiotechnology + agriculture + plant biology + smart farming + Food + Food, agriculture, forestry, water and bio-economy + plant biology + rural environment
 Agro <-  LT[c(1:2, 4, 5, 33, 45, 21, 22, 42)]
-Agro <- Agro %>% mutate(sum = rowSums(.[3:9])) %>% mutate(Agro = ifelse(sum >= 1, 1, 0))
+Agro <- Agro %>% mutate(sum = rowSums(.[3:9])) %>% mutate(agro = ifelse(sum >= 1, 1, 0))
 Agro <- Agro[c(1,2,11)]
+# Business & Industry
+Business <- LT[c(1:2, 8)]
+Business <- Business %>% mutate(sum = Business$`Business & Industry`*1) %>% mutate(busi = ifelse(sum >= 1, 1, 0))
+Business <- Business[c(1,2,5)]
 # Climate = climate action + climate adaptation strategies + climate crisis + Climate, environment, and resources
 Climate <- LT[c(1:2, 10:13)]
-Climate <- Climate %>% mutate(sum = rowSums(.[3:6])) %>% mutate(Climate = ifelse(sum >= 1, 1, 0))
+Climate <- Climate %>% mutate(sum = rowSums(.[3:6])) %>% mutate(climate = ifelse(sum >= 1, 1, 0))
 Climate <- Climate[c(1,2,8)]
 # Energy = Energy + energy transition + renewable energy
 Energy <- LT[c(1:2, 17, 18, 38)]
@@ -26,6 +30,10 @@ Ethics <- Ethics[c(1,2,9)]
 Health <- LT[c(1:2, 27, 28, 29, 32, 46, 47)]
 Health <- Health %>% mutate(sum = rowSums(.[3:8])) %>% mutate(health = ifelse(sum >= 1, 1, 0))
 Health <- Health[c(1,2,10)]
+# Policy-Makers == 1
+PM <- LT[c(1:2, 35)]
+PM <- PM %>% mutate(sum = PM$`Policy Makers`*1) %>% mutate(policy = ifelse(sum >= 1, 1, 0))
+PM <- PM[c(1:2,5)]
 # Technologies = Artificial intelligence + Digital technologies + emerging technologies + robotics
 Technologies <- LT[c(1:2, 7, 14, 16, 41)]
 Technologies <- Technologies %>% mutate(sum = rowSums(.[3:6])) %>% mutate(technologies = ifelse(sum >= 1, 1, 0))
